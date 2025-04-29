6ee546026c67b605135c006b673a379f140f5c8e
David Stewart <dstewart3919@gmail.com>
2025-04-28 22:27:58 -0400

    Full sync to latest Adding Tons of QUIZ atm.

--- ads.js
+++ ads.js
@@ -4,7 +4,7 @@
   // Load AdSense once
   if (!window.adsbygoogle) {
     const script = document.createElement('script');
-    //script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9207133906210344';
     script.async = true;
     script.crossOrigin = "anonymous";
     document.head.appendChild(script);
