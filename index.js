45336610e8b677dce04092675333208849760619
David Stewart <dstewart3919@gmail.com>
2025-11-20 20:33:17 -0500

    clean up adsense prep

--- index.js
+++
@@ -1,8 +0,0 @@
-function goToCategory() {
-  const category = document.getElementById('categorySelect').value;
-  if (!category) {
-    alert("Please choose a category.");
-    return;
-  }
-  window.location.href = `test-loader.html?genre=${encodeURIComponent(category)}`;
-}
